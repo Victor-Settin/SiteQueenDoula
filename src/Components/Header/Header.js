6402eb95aca0de00a829c8cc41e326b9a25d1370
Victor Settin <victor.profissional22@gmail.com>
2024-12-14 12:59:39 -0800

    styles4

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
       </div>
 
       <div className="menu-icon" onClick={toggleMenu}>
-        {isMenuOpen ? <HiX size={30} color="red" /> : <HiMenu size={30} color="#a2c037" />}
+        {isMenuOpen ? <HiX size={30} color="#a2c037" /> : <HiMenu size={30} color="#a2c037" />}
       </div>
 
       <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
