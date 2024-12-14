8e5ea179b998ec0c124a0eb5b8f719961ea4e75a
Victor Settin <victor.profissional22@gmail.com>
2024-12-14 14:47:40 -0800

    styles5

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
 
-      {/* Redes sociais */}
-      <div className="footer-social-links">
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
-          <FaInstagram size={26} color="#a2c037" />
-        </a>
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
-          <FaFacebook size={26} color="#a2c037" />
-        </a>
-      </div>
 
       {/* Localização */}
       <div className="footer-location">
-        <p>Location: 1234 Some Street, City, Country</p>
+        <p>Location:Some City, Country</p>
       </div>
 
       {/* Copyright */}
