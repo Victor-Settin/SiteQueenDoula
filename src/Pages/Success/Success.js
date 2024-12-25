8387937e114c4eb485893f6cd6f27b5ef892bffb
Victor Settin <victor.profissional22@gmail.com>
2024-12-25 04:43:09 -0800

    att

--- src/Pages/Success/Success.js
+++ src/Pages/Success/Success.js
@@ -11,15 +11,15 @@ const Success = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Message Sent Successfully!</h1>
-      <p>Thank you for reaching out. We'll get back to you soon.</p>
+      <p>Thank you for reaching out. I'll get back to you soon.</p>
       <button
         onClick={handleGoHome}
         style={{
           marginTop: '20px',
           padding: '10px 20px',
           fontSize: '16px',
-          backgroundColor: '#007BFF',
-          color: '#fff',
+          backgroundColor: '#29344e',
+          color: '#a2c037',
           border: 'none',
           borderRadius: '5px',
           cursor: 'pointer',
