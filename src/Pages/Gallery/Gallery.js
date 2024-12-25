4eaffe79e207f613b09f1f16841a8c6e5cec8647
Victor Settin <victor.profissional22@gmail.com>
2024-12-25 14:15:06 -0800

    att

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,91 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Gallery.css';
 import img1 from '../../Images/a1.jpg';
 import img2 from '../../Images/a2.jpg';
 import img3 from '../../Images/a3.jpg';
 import img4 from '../../Images/a4.jpg';
-import img5 from '../../Images/a5.jpg'; 
-import img6 from '../../Images/a6.jpg'; 
-import img7 from '../../Images/a7.jpg'; 
-import { motion } from 'framer-motion'; // Importa o framer-motion
+import img5 from '../../Images/a5.jpg';
+import img6 from '../../Images/a6.jpg';
+import img7 from '../../Images/a7.jpg';
+import { motion } from 'framer-motion';
+import 'react-image-lightbox/style.css';
+import Lightbox from 'react-image-lightbox';
+
+const images = [img1, img2, img3, img4, img5, img6, img7];
 
 const Gallery = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  const openLightbox = (index) => {
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
+
   return (
     <div className="gallery-container">
-      <motion.div 
+      <motion.div
         className="title"
-        initial={{ opacity: 0 }} 
-        animate={{ opacity: 1 }} 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
         Photo Gallery
       </motion.div>
 
       <div className="photos-container">
-        <motion.div 
+        <motion.div
           className="photos-grid"
-          initial={{ opacity: 0 }} 
-          animate={{ opacity: 1 }} 
-          transition={{ duration: 0.7, delay: 0.5 }} 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.7, delay: 0.5 }}
         >
-          <motion.img 
-            src={img1} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.5 }}
-          />
-          <motion.img 
-            src={img2} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.6 }}
-          />
-          <motion.img 
-            src={img5} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.7 }}
-          />
-          <motion.img 
-            src={img4} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.8 }}
-          />
-          <motion.img 
-            src={img3} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.9 }}
-          />
-          <motion.img 
-            src={img6} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.9 }}
-          />
-          <motion.img 
-            src={img7} 
-            alt="Deusa Grega" 
-            className="gallery-image"
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
-            transition={{ duration: 0.7, delay: 0.9 }}
-          />
-          </motion.div>
+          {images.map((img, index) => (
+            <motion.img
+              key={index}
+              src={img}
+              alt={`Imagem ${index + 1}`}
+              className="gallery-image"
+              onClick={() => openLightbox(index)}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.5 + index * 0.1 }}
+            />
+          ))}
+        </motion.div>
       </div>
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % images.length)
+          }
+        />
+      )}
     </div>
   );
 };
