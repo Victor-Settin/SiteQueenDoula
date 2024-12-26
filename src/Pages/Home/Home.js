1e57014526fa0674c3026db996f9cd1a58027a7b
Victor Settin <victor.profissional22@gmail.com>
2024-12-26 07:42:31 -0800

    att

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -94,29 +94,36 @@ const Home = () => {
           />
         </div>
         <div className="description-about-me">
-          <div className="content-side-about-me">
-            <motion.b
-              className="title-about-me"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 1.5 }}
-            >
-              About Me
-            </motion.b>
-            <br />
-            I am a Brazilian postpartum doula who has been living in San Mateo for over 5 years. I have been a nanny for almost 10 years caring for toddlers, babies and school-aged children. I pride myself on being loving, warm, trustworthy, loyal, empathetic and communicative. Back home I have a bachelor’s degree in accounting and two MBAs in accounting and finance, but I have found myself once again working with newborns and babies here in the United States. It is wonderful to be with families at such a special time and to be able to treat each child as the individual person they are, with respect, warmth and integrity. I am fully vaccinated and do not smoke. I have my own reliable car. I am pet-friendly and completely stigma-free. I would be happy to share more information with your family over the phone or in an interview to further discuss my qualifications and provide my references.
-          </div>
-          <motion.button
-            className="schedule-button"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 2, delay: 1.8 }}
-            onClick={handleContactClick} // Ação ao clicar
-          >
-            <AiOutlineCalendar style={{ marginRight: '8px', fontSize: '20px' }} />
-            Schedule a Free Consultation
-          </motion.button>
-        </div>
+  <div className="content-side-about-me">
+    <motion.b
+      className="title-about-me"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 1.5 }}
+    >
+      About Me
+    </motion.b>
+    <br />
+    {`I am a Brazilian postpartum doula who has been living in San Mateo for over 5 years. I have been a nanny for almost 10 years caring for toddlers, babies and school-aged children. I pride myself on being loving, warm, trustworthy, loyal, empathetic and communicative. Back home I have a bachelor’s degree in accounting and two MBAs in accounting and finance, but I have found myself once again working with newborns and babies here in the United States. It is wonderful to be with families at such a special time and to be able to treat each child as the individual person they are, with respect, warmth and integrity. I am fully vaccinated and do not smoke. I have my own reliable car. I am pet-friendly and completely stigma-free. I would be happy to share more information with your family over the phone or in an interview to further discuss my qualifications and provide my references.`
+      .split('. ')
+      .map((sentence, index) => (
+        <React.Fragment key={index}>
+          {sentence.trim()}. <br />
+        </React.Fragment>
+      ))}
+  </div>
+  <motion.button
+    className="schedule-button"
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 2, delay: 1.8 }}
+    onClick={handleContactClick} // Ação ao clicar
+  >
+    <AiOutlineCalendar style={{ marginRight: '8px', fontSize: '20px' }} />
+    Schedule a Free Consultation
+  </motion.button>
+</div>
+
       </div>
       <DoulaServices></DoulaServices>
       {/* Secção de recomendações */}
