4ad1ffd1bbb91ad479a17f24be8aad032cffd88a
Victor Settin <victor.profissional22@gmail.com>
2024-12-26 08:47:27 -0800

    att

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,17 +5,6 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      {/* Navegação no Footer */}
-      <div className="footer-navigation">
-        <ul className="footer-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/success">Success</Link></li>
-        </ul>
-      </div>
-      
       {/* Informações de Contato */}
       <div className="footer-contact">
         <p>Contact us at: <a href="mailto:raquel@turtlepurple.com">raquel@turtlepurple.com</a></p>
