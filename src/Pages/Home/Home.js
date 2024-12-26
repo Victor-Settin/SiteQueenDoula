0b8977d13683e260bd38733386c5109bd9633080
Victor Settin <victor.profissional22@gmail.com>
2024-12-25 20:00:24 -0800

    att

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Home.css';
 import deusaGregaImg from '../../Images/deusa-grega.jpg'; // Corrigido o caminho da imagem
-import cliente1 from '../../Images/deusa-grega.jpg'; // Importe imagens fictícias
-import cliente2 from '../../Images/deusa-grega.jpg';
+import cliente1 from '../../Images/img_rnt.jpg'; // Importe imagens fictícias
+import cliente2 from '../../Images/img_rnt.jpg';
 import cliente3 from '../../Images/deusa-grega.jpg';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { FaArrowDown } from 'react-icons/fa'; // Importa o ícone da seta
@@ -27,20 +27,26 @@ const Home = () => {
       name: "Renata",
       image: cliente1,
       rating: 5,
+      date: "10/16/2022", // Data individual
+      location: "San Bruno - California", // Local individual
       comment: "“When my first daughter was born I felt very alone because I didn't have any family around and Raquel helped me a lot! I'm very grateful for everything!”"
     },
     {
-      name: "Cliente2",
+      name: "Renata",
       image: cliente2,
-      rating: 4,
-      comment: "Amazing experience! Made us feel so comfortable and supported."
+      rating: 5,
+      date: "09/02/2024", // Data individual
+      location: "San Mateo - California", // Local individual
+      comment: "“Raquel had helped us a lot with my first baby and we had no doubts about being with her when our second princess arrived. We highly recommend her!”"
     },
     {
-      name: "Cliente3",
+      name: "Victor",
       image: cliente3,
-      rating: 5,
-      comment: "Highly recommend! Great attention to detail and a very warm presence."
-    }, 
+      rating: 4,
+      date: "25/08/2024", // Data individual
+      location: "Redwood City - California", // Local individual
+      comment: "Raquel made my children cry all day, she couldn't solve it, I don't recommend it ”"
+    },
     // Mais recomendações
   ];
 
@@ -59,7 +65,6 @@ const Home = () => {
     centerMode: true, // Ativa espaçamento entre os slides
     centerPadding: isSmallScreen ? '60px' : '220px', // Muda dinamicamente
   };
-  
 
   return (
     <div className="home-wrapper">
@@ -72,7 +77,6 @@ const Home = () => {
             animate={{ opacity: 1, y: 0 }} // Aparece com opacidade e se move para a posição original
             transition={{ duration: 2, ease: 'easeOut' }} // Duração e suavização
           >
-        
           </motion.div>
         </div>
       </div>
@@ -109,14 +113,7 @@ const Home = () => {
               About Me
             </motion.b>
             <br />
-            I am a Brazilian postpartum doula who has been living in San Mateo for over 5 years.
-I have been a nanny for almost 10 years caring for toddlers, babies and school-aged children. I pride myself on being loving, warm, trustworthy, loyal, empathetic and communicative.
-Back home I have a bachelor’s degree in accounting and two MBAs in accounting and finance, but I have found myself once again working with newborns and babies here in the United States.
-It is wonderful to be with families at such a special time and to be able to treat each child as the individual person they are, with respect, warmth and integrity.
-I am fully vaccinated and do not smoke.
-I have my own reliable car.
-I am pet-friendly and completely stigma-free.
-I would be happy to share more information with your family over the phone or in an interview to further discuss my qualifications and provide my references.
+            {/* Texto "About Me" */}
           </div>
           <motion.button
             className="schedule-button"
@@ -137,12 +134,21 @@ I would be happy to share more information with your family over the phone or in
         <Slider {...settings}>
           {recommendations.map((rec, index) => (
             <div key={index} className="recommendation-card">
-              <img src={rec.image} alt={rec.name} className="client-image" />
+              {rec.image ? (
+                <img src={rec.image} alt={rec.name} className="client-image" />
+              ) : (
+                <div className="client-image default-image">
+                  <i className="fas fa-user"></i> {/* Ícone de usuário */}
+                </div>
+              )}
               <h3 className="client-name">{rec.name}</h3>
               <div className="client-rating">
                 {"★".repeat(rec.rating)}{"☆".repeat(5 - rec.rating)}
               </div>
-              <div className="date-time">12/12/2024</div>
+              <div className="date-location">
+                <span className="date">{rec.date}</span> |{" "}
+                <span className="location">{rec.location}</span>
+              </div>
               <p className="client-comment">{rec.comment}</p>
             </div>
           ))}
