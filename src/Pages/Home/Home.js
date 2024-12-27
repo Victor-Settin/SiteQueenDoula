f08eaa5cb7f75910d2bf5cb52d48dff67538ffb1
Victor Settin <victor.profissional22@gmail.com>
2024-12-26 22:12:43 -0800

    att

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 import deusaGregaImg from '../../Images/deusa-grega.jpg'; // Corrigido o caminho da imagem
 import cliente1 from '../../Images/img_rnt.jpg'; // Importe imagens fictícias
@@ -8,7 +8,6 @@ import cliente4 from '../../Images/img123.jpg';
 import cliente5 from '../../Images/img111.jpg';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { FaArrowDown } from 'react-icons/fa'; // Importa o ícone da seta
-import { motion } from 'framer-motion'; // Importa o framer-motion
 import Slider from 'react-slick'; // Importa o componente Slider
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -18,12 +17,21 @@ import { useNavigate } from 'react-router-dom'; // Importar o hook useNavigate
 import Gallery from '../Gallery/Gallery';
 import Contact from '../Contact/Contact';
 import FirstimgHome from '../../Images/foto-home1.jpg';
-
+import AOS from 'aos'; // Importa a biblioteca AOS
+import 'aos/dist/aos.css'; // Importa o CSS do AOS
 
 const Home = () => {
   const navigate = useNavigate(); // Usar o hook useNavigate
 
-  // Função para redirecionar para a página de Contato
+  // Inicializa AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // Duração da animação
+      offset: 100, // Offset da animação
+      once: false, // Animação ocorre apenas uma vez
+    });
+  }, []);
+
   const handleContactClick = () => {
     navigate('/contact'); // Redireciona para a página /contact
   };
@@ -73,7 +81,6 @@ const Home = () => {
     // Mais recomendações
   ];
 
-  // Define a lógica para diferentes tamanhos de tela
   const isSmallScreen = useMediaQuery({ maxWidth: 1200 });
 
   const settings = {
@@ -82,91 +89,76 @@ const Home = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false, // Autoplay desativado
-    arrows: false, // Adiciona botões de navegação
+    autoplay: false,
+    arrows: false,
     centerMode: true,
     centerPadding: isSmallScreen ? '30px' : '220px',
   };
 
   return (
     <div className="home-wrapper">
-      <div className="content-box">
-        <img src={FirstimgHome} alt="FirstimgHome"/>
+      {/* Seção inicial */}
+      <div className="content-box" data-aos="fade-up">
+        <img src={FirstimgHome} alt="FirstimgHome" />
       </div>
 
       {/* Seta animada */}
-      <motion.div
-        className="scroll-indicator"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 1 }}
-      >
+      <div className="scroll-indicator" data-aos="fade-down">
         <FaArrowDown className="arrow-icon" />
-      </motion.div>
+      </div>
 
-    <div style={{width:"100%"}}><DoulaServices ></DoulaServices></div>
-    
+      {/* Serviços */}
+      <div style={{ width: "100%" }} data-aos="fade-right">
+        <DoulaServices />
+      </div>
 
-      <div id="about"  className="section-about-me">
-        <div className="photo-about-me">
-          <motion.img
-            src={deusaGregaImg}
-            alt="Deusa Grega"
-            className="home-image"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 1.2 }}
-          />
+      {/* About Me */}
+      <div id="about" className="section-about-me" data-aos="fade-left">
+        <div className="photo-about-me" data-aos="zoom-in">
+          <img src={deusaGregaImg} alt="Deusa Grega" className="home-image" />
         </div>
-        
         <div className="description-about-me">
-          
-  <div className="content-side-about-me">
-    <motion.b
-      className="title-about-me"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1, delay: 1.5 }}
-    >
-      About Me
-    </motion.b>
-    <br />
-    <div className='text-about-me'>
-    {`I am a Brazilian postpartum doula who has been living in San Mateo for over 5 years. I have been a nanny for almost 10 years caring for toddlers, babies and school-aged children. I pride myself on being loving, warm, trustworthy, loyal, empathetic and communicative. Back home I have a bachelor’s degree in accounting and two MBAs in accounting and finance, but I have found myself once again working with newborns and babies here in the United States. It is wonderful to be with families at such a special time and to be able to treat each child as the individual person they are, with respect, warmth and integrity. I am fully vaccinated and do not smoke. I have my own reliable car. I am pet-friendly and completely stigma-free. I would be happy to share more information with your family over the phone or in an interview to further discuss my qualifications and provide my references.`
-     }
+          <div className="content-side-about-me">
+            <b className="title-about-me">About Me</b>
+            <p className="text-about-me">
+            I am a Brazilian postpartum doula who has been living in San Mateo for over 5 years.
+I have been a nanny for almost 10 years caring for toddlers, babies and school-aged children. I pride myself on being loving, warm, trustworthy, loyal, empathetic and communicative.
+Back home I have a bachelor’s degree in accounting and two MBAs in accounting and finance, but I have found myself once again working with newborns and babies here in the United States.
+It is wonderful to be with families at such a special time and to be able to treat each child as the individual person they are, with respect, warmth and integrity.
+I am fully vaccinated and do not smoke.
+I have my own reliable car.
+I am pet-friendly and completely stigma-free.
+I would be happy to share more information with your family over the phone or in an interview to further discuss my qualifications and provide my references.
+            </p>
+            <button
+              className="schedule-button"
+              onClick={handleContactClick}
+              data-aos="fade-up"
+            >
+              <AiOutlineCalendar style={{ marginRight: '8px', fontSize: '20px' }} />
+              Schedule a Free Consultation
+            </button>
+          </div>
+        </div>
       </div>
-  </div>
-  <motion.button
-    className="schedule-button"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 2, delay: 1.8 }}
-    onClick={handleContactClick} // Ação ao clicar
-  >
-    <AiOutlineCalendar style={{ marginRight: '8px', fontSize: '20px' }} />
-    Schedule a Free Consultation
-  </motion.button>
-</div>
 
+      {/* Galeria */}
+      <div id="gallery" style={{ width: "100%" }} data-aos="zoom-out">
+        <Gallery />
       </div>
-      
-      {/* Secção de recomendações */}
 
-      <div id="gallery" style={{width:"100%"}}><Gallery></Gallery></div>
-      <div id="contact" style={{width:"100%"}}><Contact></Contact></div> 
+      {/* Contato */}
+      <div id="contact" style={{ width: "100%" }} data-aos="fade-up">
+        <Contact />
+      </div>
 
-      <div className="recommendations-section">
+      {/* Recomendações */}
+      <div className="recommendations-section" data-aos="fade-up">
         <h2 className="section-title">What Our Clients Say</h2>
         <Slider {...settings}>
           {recommendations.map((rec, index) => (
-            <div key={index} className="recommendation-card">
-              {rec.image ? (
-                <img src={rec.image} alt={rec.name} className="client-image" />
-              ) : (
-                <div className="client-image default-image">
-                  <i className="fas fa-user"></i> {/* Ícone de usuário */}
-                </div>
-              )}
+            <div key={index} className="recommendation-card" data-aos="zoom-in">
+              <img src={rec.image} alt={rec.name} className="client-image" />
               <h3 className="client-name">{rec.name}</h3>
               <div className="client-rating">
                 {"★".repeat(rec.rating)}{"☆".repeat(5 - rec.rating)}
