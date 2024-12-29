9a4ac1d5416ea3cbbf51e0884c533e0aefda33be
Victor Settin <victor.profissional22@gmail.com>
2024-12-28 22:11:39 -0800

    att

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { HiMenu, HiX } from 'react-icons/hi';
 import './Header.css';
@@ -7,17 +7,19 @@ import logo from '../../Images/header-logo-3.png';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const location = useLocation(); // Hook para detectar mudanças de rota
 
   // Função para alternar o estado do menu
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Fechar o menu automaticamente quando a rota mudar
-  useEffect(() => {
-    setIsMenuOpen(false);
-  }, [location]); // Quando a localização mudar (i.e., quando a rota mudar), fecha o menu
+  // Função para rolar suavemente até a seção
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <header className="header">
@@ -26,13 +28,12 @@ const Header = () => {
           <img src={logo} alt="Logo" className="logo-pc" />
           <img src={logo} alt="Logo" className="logo-mobile" />
         </NavLink>
-        {/* Linha curvada abaixo do logo */}
         <div className="curved-line-container-1">
           <svg viewBox="0 0 100 20" xmlns="http://www.w3.org/2000/svg" className="curved-line">
             <path d="M 0 20 Q 50 10, 100 20" stroke="#a1cf4a" fill="transparent" strokeWidth="1" />
           </svg>
         </div>
-      </div>    
+      </div>
 
       <div className="menu-icon" onClick={toggleMenu}>
         <div className="menu-icon-wrapper">
@@ -40,29 +41,18 @@ const Header = () => {
         </div>
       </div>
 
-
       <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
-        <NavLink
-          to="/"
-          className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/gallery"
-          className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}
-        >
+        <div onClick={() => scrollToSection('about')} className="nav-link">
+          About Me
+        </div>
+        <div onClick={() => scrollToSection('gallery')} className="nav-link">
           Photos Gallery
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}
-        >
+        </div>
+        <div onClick={() => scrollToSection('contact')} className="nav-link">
           Contact Us
-        </NavLink>
+        </div>
       </nav>
 
-      {/* Linha curvada antes das redes sociais */}
       <div className="curved-line-container-2">
         <svg viewBox="0 0 200 50" xmlns="http://www.w3.org/2000/svg" className="curved-line-2">
           <path d="M 0 50 Q 100 20, 200 50" stroke="#a1cf4a" fill="transparent" strokeWidth="1" />
