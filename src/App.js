54b46db0205a9ad972a840f3a6f7fd41bc0159f3
Victor Settin <victor.profissional22@gmail.com>
2025-01-06 18:33:17 -0800

    att

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './Pages/Home/Home';
 import Header from './Components/Header/Header';
-
 import Contact from './Pages/Contact/Contact';
 import Gallery from './Pages/Gallery/Gallery';
 import Footer from './Components/Footer/Footer';
@@ -14,13 +14,15 @@ function App() {
       <div className="App">
         <Header />
         {/* Ajuste para garantir altura total da página */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/success" element={<Success />} />
-            </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
         <Footer />
+        {/* Componente Analytics para rastrear visitantes */}
+        <Analytics />
       </div>
     </Router>
   );
