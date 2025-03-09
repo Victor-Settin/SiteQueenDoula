254522db9dc051899d1217435198c54f68136fed
Victor Settin <victor.profissional22@gmail.com>
2025-03-08 22:24:37 -0800

    alter name

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
 
   const recommendations = [
     {
-      name: "Renata",
+      name: "Sarah",
       image: cliente1,
       rating: 5,
       date: "10/16/2022", // Data individual
@@ -66,7 +66,7 @@ const Home = () => {
       comment: "“I had my baby in November 2023 and I don't know what my postpartum period would have been like without Raquel's help. She was essential for my recovery and the baby's well-being. Postpartum was the most difficult time of my life and it was great to have someone so experienced and respectful helping me recover and take care of my baby. She taught me how to bathe my son, how to keep him safe, and took care of him so I could rest. Raquel, thank you so much for everything!”"
     },
     {
-      name: "Renata",
+      name: "Sarah",
       image: cliente2,
       rating: 5,
       date: "09/02/2024", // Data individual
